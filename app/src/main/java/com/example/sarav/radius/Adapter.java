be7a205bf5840e20f32e8ca2ab74b1e615617221
mk1999 <keerthanamahendiren@gmail.com>
2019-04-11 09:51:02 +0530

    Update Adapter.java

--- app/src/main/java/com/example/sarav/radius/Adapter.java
+++ app/src/main/java/com/example/sarav/radius/Adapter.java
@@ -2,42 +2,19 @@ package com.example.sarav.radius;
 
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AlertDialog;
-import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
-import java.net.URL;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
 
 public class Adapter extends ArrayAdapter<listview> {
 
@@ -66,41 +43,14 @@ public class Adapter extends ArrayAdapter<listview> {
         ImageView img = view.findViewById(R.id.img);
 
         listview ad1 = adList.get(position);
-
         name.setText(ad1.getName());
         age.setText(ad1.getAge());
-        new DownLoadImageTask(img).execute(ad1.getPic());
+       
+        Glide.with(getContext())
+                .load(ad1.getPic())
+                .into(img);
 
         return view;
     }
-
-
-    private class DownLoadImageTask extends AsyncTask<String,Void,Bitmap> {
-        ImageView imageView;
-
-        public DownLoadImageTask(ImageView imageView){
-            this.imageView = imageView;
-        }
-
-
-        protected Bitmap doInBackground(String...urls){
-            String urlOfImage = urls[0];
-            Bitmap logo = null;
-            try{
-                InputStream is = new URL(urlOfImage).openStream();
-
-                logo = BitmapFactory.decodeStream(is);
-            }catch(Exception e){
-                e.printStackTrace();
-            }
-            return logo;
-        }
-
-        protected void onPostExecute(Bitmap result){
-            imageView.setImageBitmap(result);
-        }
-    }
-
-
 
 }
